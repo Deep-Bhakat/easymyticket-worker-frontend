911e264b51b1619ae654f704bb5b67bffc6ea6a3
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-02-27 20:55:58 +0530

    dashboard screen

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Screens
 import LoginScreen from './screens/LoginScreen';
-
+import DashboardScreen from './screens/DashboardScreen';
 
 const App = () => {
 	return <>
 		<BrowserRouter>
 			<Switch>
 				<Route exact path='/login' component={LoginScreen} />
+				<Route exact path='/dashboard' component={DashboardScreen} />
 			</Switch>
 		</BrowserRouter>
 	</>;
---
+++ src/screens/DashboardScreen.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+
+// Contexts
+import WorkerContext from '../contexts/worker-context';
+
+const DashboardScreen = (props) => {
+    const workerCtx = useContext(WorkerContext);
+    
+    useEffect(() => {
+        // Redirect to login screen if not logged in
+        if(!workerCtx.loggedInWorker) {
+            props.history.push('/login');
+        }
+    }, [workerCtx, props.history]);
+    
+    return <div className='bg-gradient'>
+        <Container>
+            <h1>Welcome, {workerCtx.loggedInWorker && workerCtx.loggedInWorker.name}</h1>
+            <button className='btn_action my-5'>Scan tickets</button>
+            <button className='btn_action my-5'>Logout</button>
+        </Container>
+    </div>
+}
+
+export default DashboardScreen;
