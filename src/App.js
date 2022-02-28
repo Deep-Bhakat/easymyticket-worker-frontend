418569a4052ec22e1c4af343edfde15859e33cb5
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-02-28 17:23:13 +0530

    route to qr scanner screen

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // Screens
 import LoginScreen from './screens/LoginScreen';
 import DashboardScreen from './screens/DashboardScreen';
+import ScanTicketScreeen from './screens/ScanTicketScreen';
 
 const App = () => {
 	return <>
@@ -11,6 +12,7 @@ const App = () => {
 			<Switch>
 				<Route exact path='/login' component={LoginScreen} />
 				<Route exact path='/dashboard' component={DashboardScreen} />
+				<Route exact path='/scanticket' component={ScanTicketScreeen} />
 			</Switch>
 		</BrowserRouter>
 	</>;
