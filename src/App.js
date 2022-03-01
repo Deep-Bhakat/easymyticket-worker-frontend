20781b1de4d9e75c5e267020eeccabbc16319117
Piyush Saha <itsmepiyushsaha@gmail.com>
2022-03-01 14:00:26 +0530

    root route / redirect to login or dashboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+// Contexts
+import WorkerContext from './contexts/worker-context';
 
 // Screens
 import LoginScreen from './screens/LoginScreen';
@@ -7,9 +10,15 @@ import DashboardScreen from './screens/DashboardScreen';
 import ScanTicketScreeen from './screens/ScanTicketScreen';
 
 const App = () => {
+	const workerCtx = useContext(WorkerContext);
+	
 	return <>
 		<BrowserRouter>
 			<Switch>
+				// Root route / redirect
+				<Route path='/' exact>
+					{workerCtx.loggedInWorker ? <Redirect to='/login' /> : <Redirect to='dashboard' /> }
+				</Route>
 				<Route exact path='/login' component={LoginScreen} />
 				<Route exact path='/dashboard' component={DashboardScreen} />
 				<Route exact path='/scanticket' component={ScanTicketScreeen} />
